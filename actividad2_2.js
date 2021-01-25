5f3d7a69727ad2a654fb8dc4d39564a1b993cd98
JesusJahir <70489023+JesusJahir@users.noreply.github.com>
2021-01-24 19:37:44 -0600

    Add files via upload

---
+++ actividad2_2.js
@@ -0,0 +1,44 @@
+[
+  {
+    "nombre": "Katy",
+    "hobby": "Cantar",
+    "edad": 36,
+    "telefono": 3315447203,
+    "direccion": "Santa Barbara #10, Candyfornia, USA"
+  },
+  {
+    "nombre": "Harry",
+    "hobby": "Tejer",
+    "edad": 26,
+    "telefono": 5472997014 ,
+    "direccion": "Fine Line Street, Chesire, UK"
+  },
+  {
+    "nombre": "Adele",
+    "hobby": "Hacer ejercicio",
+    "edad": 32,
+    "telefono": 5472015084,
+    "direccion": "21 street, London, UK"
+  },
+  {
+    "nombre": "Ava",
+    "hobby": "Leer",
+    "edad": 26,
+    "telefono": 7229082053,
+    "direccion": "Heaven and Hell, Wisconsin, USA"
+  },
+  {
+    "nombre": "Abel",
+    "hobby": "Conducir",
+    "edad": 30,
+    "telefono": 2014725161,
+    "direccion": "Blinding Lights Street, Ontario, Canada"
+  },
+  {
+    "nombre": "Taylor",
+    "hobby": "Cocinar",
+    "edad": 31,
+    "telefono": 8541524173,
+    "direccion": "Folklore, Evermore, USA"
+  }
+]
